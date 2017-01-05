679d5b264c606338f40faf9aac60426fb5bb59c7
Renata Maraj <renatavmaraj@gmail.com>
2017-01-04 20:25:01 -0800

    Implement models views and collections

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -5,7 +5,15 @@ var PlayerView = Backbone.View.extend({
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
   el: '<audio controls autoplay />',
 
+
+
   initialize: function() {
+    console.log(this.$el);
+    this.$el.on('ended', function() {
+
+      this.model.ended();
+      //this.collection.playFirst();
+    }.bind(this))
   },
 
   setSong: function(song) {
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,22 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName : 'table',
+
   initialize: function() {
+    this.render();
+
   },
 
   render: function() {
-    return this.$el;
+    console.log("this is thiscollection", this.collection);
+    this.$el.children().detach();
+
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
